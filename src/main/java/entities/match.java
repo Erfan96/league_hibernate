e9affc05fce7696b04dd5119c4d86d1de65980f6
efsam <E.salimi75@ymail.com>
2020-12-26 16:20:09 +0330

    commit match.java in secondary

---
+++ src/main/java/entities/match.java
@@ -0,0 +1,37 @@
+package entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "match")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "season", nullable = false)
+    private Integer season;
+
+    @Column(name = "date_time", nullable = false)
+    private Timestamp date;
+
+    @OneToOne
+    @JoinColumn(name = "stadium_id", nullable = false)
+    private Stadium stadium;
+
+    @OneToOne
+    @JoinColumn(name = "home_team_id", nullable = false)
+    private Team homeTeam;
+
+    @OneToOne
+    @JoinColumn(name = "away_team_id", nullable = false)
+    private Team awayTeam;
+}
