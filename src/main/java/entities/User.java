7996af76751f528903183da83399664540238f52
efsam <E.salimi75@ymail.com>
2020-12-26 11:55:36 +0330

    commit User in secondary

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,32 @@
+package entities;
+
+import lombok.*;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+@Getter @Setter
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String fName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lName;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "team_id", nullable = false)
+    private Integer teamId;
+
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled;
+}
