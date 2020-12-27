591c019c1efda448b22efefe09c10db46587be54
efsam <E.salimi75@ymail.com>
2020-12-27 16:43:17 +0330

    commit ContractDao.java to secondary

---
+++ src/main/java/dao/ContractDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import entities.Contract;
+import javax.persistence.EntityManager;
+
+public class ContractDao extends EntityDao<Contract, Integer> {
+
+    public ContractDao(EntityManager entityManager) {
+        super(entityManager);
+    }
+
+    public Class<Contract> getEntityClass() {
+        return Contract.class;
+    }
+}
