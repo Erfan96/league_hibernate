b4d702641acdf94eb6e13c0330edbb3cbe2a40ea
efsam <E.salimi75@ymail.com>
2020-12-28 17:05:43 +0330

    commit App.java in secondary

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,8 @@ import java.sql.Date;
 import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 public class App {
 
@@ -26,120 +28,173 @@ public class App {
         initializeDao(entityManager);
         entityManager.getTransaction().begin();
 
-        City city = new City();
-        city.setName("Tehran");
-        cityDao.save(city);
+//        City city = new City();
+//        city.setName("Tehran");
+//        cityDao.save(city);
+//
+//        City city2 = new City();
+//        city2.setName("Shiraz");
+//        cityDao.save(city2);
+//
+//        Stadium stadium = new Stadium();
+//        stadium.setCity(city);
+//        stadium.setCapacity(72000);
+//        stadium.setName("Azadi");
+//        stadiumDao.save(stadium);
+//
+//
+//        Stadium stadium2 = new Stadium();
+//        stadium2.setName("Fajr");
+//        stadium2.setCapacity(10000);
+//        stadium2.setCity(city2);
+//        stadiumDao.save(stadium2);
+//
+//        Team team = new Team();
+//        team.setName("Persepolis");
+//        team.setCity(city);
+//        team.setStadium(stadium);
+//        teamDao.save(team);
+//
+//        Team team2 = new Team();
+//        team2.setName("Bargh");
+//        team2.setCity(city2);
+//        team2.setStadium(stadium2);
+//        teamDao.save(team2);
+//
+//        User user = new User();
+//        user.setFName("Ali");
+//        user.setLName("Karimi");
+//        user.setType("Player");
+//        user.setPosition("Forward");
+//        user.setTeam(team);
+//        user.setEnabled(true);
+//        userDao.save(user);
+//
+//        User user2 = new User();
+//        user2.setFName("Fazel");
+//        user2.setLName("Larijani");
+//        user2.setType("Player");
+//        user2.setPosition("Forward");
+//        user2.setTeam(team2);
+//        user2.setEnabled(true);
+//        userDao.save(user2);
+//
+//        Contract contract = new Contract();
+//        contract.setUser(user);
+//        contract.setSeason(2015);
+//        contract.setSalary(250.5);
+//        contract.setTeam(team);
+//        contract.setYears(5);
+//        contract.setDate(parseDate("2015-11-15"));
+//        contract.setEnabled(true);
+//        contractDao.save(contract);
+//
+//        Contract contract2 = new Contract();
+//        contract2.setUser(user2);
+//        contract2.setSeason(2018);
+//        contract2.setSalary(350.5);
+//        contract2.setTeam(team2);
+//        contract2.setYears(3);
+//        contract2.setDate(parseDate("2018-07-04"));
+//        contract2.setEnabled(true);
+//        contractDao.save(contract2);
+//
+//        Matches match = new Matches();
+//        match.setSeason(2019);
+//        match.setStadium(stadium);
+//        match.setHomeTeam(team);
+//        match.setAwayTeam(team2);
+//        match.setDateTime(parseTimestamp("2019-02-11 20:15:00"));
+//        matchDao.save(match);
+//
+//        Result result = new Result();
+//        result.setTeamId(team);
+//        result.setMatches(match);
+//        result.setGoal(2);
+//        result.setScore(3);
+//        result.setPlace("Home");
+//        resultDao.save(result);
+//
+//        Result result2 = new Result();
+//        result2.setTeamId(team2);
+//        result2.setMatches(match);
+//        result2.setGoal(1);
+//        result2.setScore(0);
+//        result2.setPlace("away");
+//        resultDao.save(result2);
+//
+//        Set<User> users = new HashSet<User>();
+//        users.add(user);
+//        users.add(user2);
+//
+//        MatchEvent matchEvent0 = new MatchEvent();
+//        matchEvent0.setMatches(match);
+//        matchEvent0.setType("Lineup");
+//        matchEvent0.setDateTime(parseTimestamp("2019-02-11 20:15:00"));
+//        matchEvent0.setUsers(users);
+//        matchEventDao.save(matchEvent0);
+//
+//        Set<User> users2 = new HashSet<User>();
+//        users2.add(user);
+//
+//        MatchEvent matchEvent = new MatchEvent();
+//        matchEvent.setMatches(match);
+//        matchEvent.setType("Goal");
+//        matchEvent.setDateTime(parseTimestamp("2019-02-11 20:30:00"));
+//        matchEvent.setUsers(users2);
+//        matchEventDao.save(matchEvent);
+//
+//        Set<User> users3 = new HashSet<User>();
+//        users3.add(user2);
+//
+//        MatchEvent matchEvent2 = new MatchEvent();
+//        matchEvent2.setMatches(match);
+//        matchEvent2.setType("Goal");
+//        matchEvent2.setDateTime(parseTimestamp("2019-02-11 20:52:26"));
+//        matchEvent2.setUsers(users3);
+//        matchEventDao.save(matchEvent2);
+//
+//        Set<User> users4 = new HashSet<User>();
+//        users4.add(user);
+//
+//        MatchEvent matchEvent3 = new MatchEvent();
+//        matchEvent3.setMatches(match);
+//        matchEvent3.setType("Goal");
+//        matchEvent3.setDateTime(parseTimestamp("2019-02-11 21:33:45"));
+//        matchEvent3.setUsers(users4);
+//        matchEventDao.save(matchEvent3);
 
-        City city2 = new City();
-        city2.setName("Shiraz");
-        cityDao.save(city2);
+//        User user = userDao.load(3);
+//        Team team = teamDao.load(2);
+//        Contract contract = new Contract();
+//        contract.setUser(user);
+//        contract.setTeam(team);
+//        contract.setSalary(200.0);
+//        contract.setDate(parseDate("2018-10-22"));
+//        contract.setYears(1);
+//        contract.setEnabled(true);
+//        contractDao.save(contract);
 
-        Stadium stadium = new Stadium();
-        stadium.setCity(city);
-        stadium.setCapacity(72000);
-        stadium.setName("Azadi");
-        stadiumDao.save(stadium);
+//        Team team = teamDao.load(2);
+//        User user = new User();
+//        user.setFName("Hashem");
+//        user.setLName("Akrami");
+//        user.setType("Player");
+//        user.setPosition("Goalkeeper");
+//        user.setTeam(team);
+//        user.setEnabled(true);
+//        userDao.save(user);
 
-        Stadium stadium2 = new Stadium();
-        stadium2.setName("Fajr");
-        stadium2.setCapacity(10000);
-        stadium2.setCity(city2);
-        stadiumDao.save(stadium2);
 
-        Team team = new Team();
-        team.setName("Persepolis");
-        team.setCity(city);
-        team.setStadium(stadium);
-        teamDao.save(team);
 
-        Team team2 = new Team();
-        team2.setName("Bargh");
-        team2.setCity(city2);
-        team2.setStadium(stadium2);
-        teamDao.save(team2);
+        User user = userDao.load(3);
+        System.out.println(user.getLName());
 
-        User user = new User();
-        user.setFName("Ali");
-        user.setLName("Karimi");
-        user.setType("Player");
-        user.setPosition("Forward");
-        user.setTeam(team);
-        user.setEnabled(true);
-        userDao.save(user);
+//        user.setLName("Akbari");
+//        userDao.update(user);
 
-        User user2 = new User();
-        user2.setFName("Fazel");
-        user2.setLName("Larijani");
-        user2.setType("Player");
-        user2.setPosition("Forward");
-        user2.setTeam(team2);
-        user2.setEnabled(true);
-        userDao.save(user2);
+        userDao.delete(user);
 
-        Contract contract = new Contract();
-        contract.setUser(user);
-        contract.setSeason(2015);
-        contract.setSalary(250.5);
-        contract.setTeam(team);
-        contract.setYears(5);
-        contract.setDate(parseDate("2015-11-15"));
-        contract.setEnabled(true);
-        contractDao.save(contract);
-
-        Contract contract2 = new Contract();
-        contract2.setUser(user2);
-        contract2.setSeason(2018);
-        contract2.setSalary(350.5);
-        contract2.setTeam(team2);
-        contract2.setYears(3);
-        contract2.setDate(parseDate("2018-07-04"));
-        contract2.setEnabled(true);
-        contractDao.save(contract2);
-
-        Match match = new Match();
-        match.setSeason(2019);
-        match.setStadium(stadium);
-        match.setHomeTeam(team);
-        match.setAwayTeam(team2);
-        match.setDateTime(parseTimestamp("2019-02-11 20:15:00"));
-        matchDao.save(match);
-
-        Result result = new Result();
-        result.setTeamId(team);
-        result.setMatch(match);
-        result.setGoal(2);
-        result.setScore(3);
-        result.setPlace("Home");
-        resultDao.save(result);
-
-        Result result2 = new Result();
-        result2.setTeamId(team2);
-        result2.setMatch(match);
-        result2.setGoal(1);
-        result2.setScore(0);
-        result2.setPlace("away");
-        resultDao.save(result2);
-
-        MatchEvent matchEvent = new MatchEvent();
-        matchEvent.setMatch(match);
-        matchEvent.setType("Goal");
-        matchEvent.setDateTime(parseTimestamp("2019-02-11 20:30:00"));
-        matchEvent.setUser(user);
-        matchEventDao.save(matchEvent);
-
-        MatchEvent matchEvent2 = new MatchEvent();
-        matchEvent2.setMatch(match);
-        matchEvent2.setType("Goal");
-        matchEvent2.setDateTime(parseTimestamp("2019-02-11 20:52:26"));
-        matchEvent2.setUser(user2);
-        matchEventDao.save(matchEvent2);
-
-        MatchEvent matchEvent3 = new MatchEvent();
-        matchEvent3.setMatch(match);
-        matchEvent3.setType("Goal");
-        matchEvent3.setDateTime(parseTimestamp("2019-02-11 21:33:45"));
-        matchEvent3.setUser(user);
-        matchEventDao.save(matchEvent3);
 
         entityManager.getTransaction().commit();
         entityManager.close();
