a014d4c1cc36aec41bbd0af2e08e8dc6fd00f26d
efsam <E.salimi75@ymail.com>
2021-01-09 22:59:35 +0330

    commit ResultDao.java in secondary

--- src/main/java/dao/ResultDao.java
+++ src/main/java/dao/ResultDao.java
@@ -41,4 +41,23 @@ public class ResultDao extends EntityDao<Result, Integer> {
         list.forEach(r ->
                 System.out.println(r.get(0) + " --> " + r.get(1)));
     }
+
+    public void getChampion(){
+        CriteriaBuilder cb = getCriteriaBuilder();
+        CriteriaQuery<Tuple> criteria = cb.createTupleQuery();
+        Root<Result> fromResult = criteria.from(Result.class);
+
+        Join<Team, Result> teamResultJoin = fromResult.join("teamId");
+        Join<Result, Matches> join2 = fromResult.join("matches");
+
+        criteria.multiselect(teamResultJoin.get("name"), cb.sum(fromResult.get("score")))
+                .where(cb.equal(join2.get("season"), "2019"));
+        criteria.groupBy(teamResultJoin.get("id"));
+        criteria.orderBy(cb.desc(cb.sum(fromResult.get("score"))));
+
+        TypedQuery<Tuple> typedQuery = entityManager.createQuery(criteria).setMaxResults(1);
+        Tuple list = typedQuery.getSingleResult();
+
+        System.out.println(list);
+    }
 }
